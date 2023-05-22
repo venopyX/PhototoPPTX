0b26411f7f0a6a179d59dfd5f01b73df15d9f6e1
Gemechis Chala Degefa <117309484+gladsonchala@users.noreply.github.com>
2023-05-22 13:28:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#TOKEN = '6027255433:AAEOSUy7MSQyF1zThWHnkXUkTnjWpu0Wxsw'
+
+
 import os
 import requests
 from PIL import Image
@@ -8,14 +11,13 @@ import telebot
 # Replace with your Telegram bot token
 TOKEN = '6027255433:AAEOSUy7MSQyF1zThWHnkXUkTnjWpu0Wxsw'
 
-
 # Create a new Telebot instance
 bot = telebot.TeleBot(TOKEN)
 
 # Define a function to handle the /start command
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.reply_to(message, 'Welcome to the PowerPoint bot! Please send me an image and I will create a PowerPoint presentation with it.')
+    bot.reply_to(message, 'Welcome to the PowerPoint bot! Please send me an image and I will create a PowerPoint presentation with it as the background.')
 
 # Define a function to handle incoming messages
 @bot.message_handler(content_types=['photo'])
@@ -35,8 +37,13 @@ def handle_message(message):
         width, height = image.size
         prs = Presentation()
         slide = prs.slides.add_slide(prs.slide_layouts[6])
+        slide.background.fill.solid()
+        slide.background.fill.fore_color.rgb = (255, 255, 255)
         left = top = 0
-        slide.shapes.add_picture('image.jpg', left, top, width=prs.slide_width, height=prs.slide_height)
+        slide.shapes.add_picture('image.jpg', left, top, width=Inches(10), height=Inches(7.5))
+        # Add a text box to the slide
+        textbox = slide.shapes.add_textbox(Inches(0), Inches(6.5), Inches(10), Inches(0.5))
+        textbox.text = 'Created with: GCPPT_bot'
         # Save the PowerPoint presentation to a file
         prs.save('presentation.pptx')
         # Send the PowerPoint presentation back to the user
@@ -48,7 +55,7 @@ def handle_message(message):
 # Define a function to handle errors
 @bot.message_handler(func=lambda message: True)
 def handle_error(message):
-    bot.reply_to(message, 'Sorry, I could not process your request. Please send me an image and I will create a PowerPoint presentation with it.')
+    bot.reply_to(message, 'Sorry, I could not process your request. Please send me an image and I will create a PowerPoint presentation with it as the background.')
 
 # Start the bot
 bot.polling()
